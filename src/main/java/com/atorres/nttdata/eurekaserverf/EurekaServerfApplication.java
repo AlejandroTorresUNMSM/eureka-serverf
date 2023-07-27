b2c00931a5560bd8a200dcde2bc9b8f77002a2e4
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-27 10:44:07 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/eurekaserverf/EurekaServerfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.eurekaserverf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerfApplication.class, args);
+	}
+
+}
